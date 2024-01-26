7e0ab40cab4359f193fdb95b8c481e4ffe1690d6
christian.baumann@maibornwolff.de <christian.baumann@maibornwolff.de>
2024-01-26 15:35:31 +0100

    wip

--- webinar_3___supervised_learning/part_6___support_vector_machines/impl_1___example_svms/xxx.py
+++ webinar_3___supervised_learning/part_6___support_vector_machines/impl_1___example_svms/xxx.py
@@ -10,20 +10,28 @@ def main():
     # Create samples.
     pass  # TODO
 
+    # Create samples.
+    # features, targets = make_blobs(n_samples=100, centers=2, cluster_std=1.0)
+    # features, targets = make_circles(n_samples=100, noise=0.1, factor=0.1)
+    features, targets = make_moons(n_samples=1000, noise=0.2)
+
     # scaled support vector classifier
-    # TODO
+    # scaled_svc = make_pipeline(StandardScaler(), LinearSVC(C=1_000_000, dual='auto'))
+    # scaled_svc = make_pipeline(StandardScaler(), SVC(kernel='rbf', C=100_000))
+    scaled_svc = make_pipeline(StandardScaler(), SVC(kernel='poly', C=10_000, degree=3, coef0=1))
+    scaled_svc.fit(features, targets)
 
     # visualization
-    # fig, ax = plt.subplots(dpi=300, layout='constrained')
-    # ax.scatter(features[:, 0], features[:, 1], c=targets)
-    # x_limits = ax.get_xlim()
-    # y_limits = ax.get_ylim()
-    # grid_x, grid_y = np.meshgrid(np.linspace(x_limits[0], x_limits[1], num=100 + 1),
-    #                              np.linspace(y_limits[0], y_limits[1], num=100 + 1))
-    # all_x_y_pairs = np.array((grid_x, grid_y)).T.reshape(-1, 2, order='F')
-    # decision_function_values = scaled_svc.decision_function(all_x_y_pairs).reshape(grid_x.shape)
-    # ax.contour(grid_x, grid_y, decision_function_values, levels=[0], colors='gray')
-    # fig.show()  # or plt.show()
+    fig, ax = plt.subplots(dpi=300, layout='constrained')
+    ax.scatter(features[:, 0], features[:, 1], c=targets)
+    x_limits = ax.get_xlim()
+    y_limits = ax.get_ylim()
+    grid_x, grid_y = np.meshgrid(np.linspace(x_limits[0], x_limits[1], num=100 + 1),
+                                 np.linspace(y_limits[0], y_limits[1], num=100 + 1))
+    all_x_y_pairs = np.array((grid_x, grid_y)).T.reshape(-1, 2, order='F')
+    decision_function_values = scaled_svc.decision_function(all_x_y_pairs).reshape(grid_x.shape)
+    ax.contour(grid_x, grid_y, decision_function_values, levels=[0], colors='gray')
+    fig.show()  # or plt.show()
 
 
 if __name__ == '__main__':
